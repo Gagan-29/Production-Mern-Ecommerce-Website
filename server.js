801264b851eba1dd6b271c4a013bc69bf5acc3e7
Gagan-29 <122251921+Gagan-29@users.noreply.github.com>
2024-03-08 17:00:20 +0530

    updated

--- server.js
+++ server.js
@@ -7,14 +7,9 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
-import path from "path";
-import { fileURLToPath } from 'url';
 dotenv.config();
 //database config
 connectDB();
-//es module fix
-const __filename=fileURLToPath(import.meta.url);
-const __dirname=path.dirname(__filename);
 //rest object
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -22,14 +17,14 @@ const PORT = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, "./client/build")));
+
 //routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 //rest api
-app.use("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+app.get("/", async (req, res) => {
+  res.send("<h1>Welcome to MERN stack project.</h1>");
 });
 
 app.listen(PORT, () => {
